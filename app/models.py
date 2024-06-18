18f85bdffa3b3c4c5260537299900fb1d1e7e4c4
IlyaKHalilov <ilyakhalilov1@gmail.com>
2024-06-18 15:41:16 +0600

    'basehtml'

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class MyPerson(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.PositiveSmallIntegerField()
+    gender = models.PositiveSmallIntegerField(
+        choices=(
+            (1, 'Не указано'),
+            (2, 'Мужчина'),
+            (3, 'Женщина'),
+        )
+    )
+    photo = models.FileField(upload_to='main_photos/')
+    majority = models.CharField(max_length=200)
+    short_description = models.TextField()
+
+
+class MyPersonDetail(models.Model):
+    person = models.ForeignKey(MyPerson, on_delete=models.PROTECT)
+    full_description = models.TextField()
+    quote = models.TextField()
